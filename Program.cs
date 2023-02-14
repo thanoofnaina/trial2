c28e4c6ae5d268ea02fd6c541cf6e840bc5500e0
thanoofnaina <thanoofnaina@gmail.com>
2023-02-14 16:31:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter Your Name: ");
+string realName = Console.ReadLine();
+string reverseName = "";
+
+for (int i = realName.Length - 1; i >= 0 ; i--)
+{
+    reverseName = reverseName + realName[i];
+}
+
+Console.WriteLine(reverseName);
